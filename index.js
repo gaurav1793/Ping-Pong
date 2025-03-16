726d4f4b40ce39c28be846f41cc4c854e1d2f54d
Gaurav <katariagaurav686@gmail.com>
2025-03-16 14:37:22 +0530

    making the paddle move

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     let table = document.getElementById('ping_pong_table');
     let ball = document.getElementById('ball');
+    let handle = document.getElementById('handle');
+
 
     //initiall co-ord of ball
-    let ballx=10;
+    let ballx=400;
     let bally=30;
     ball.style.left=`${ballx}px`;
     ball.style.top=`${bally}px`;
@@ -20,14 +22,26 @@ document.addEventListener("DOMContentLoaded",()=>{
         ball.style.left=`${ballx}px`;
         ball.style.top=`${bally}px`;
 
-        if(ballx>table.offsetWidth-ball.offsetWidth || ballx<=0)dx*=-1;
+        if(ballx>table.offsetWidth-ball.offsetWidth-18 || ballx<=18)dx*=-1;
         if(bally>table.offsetHeight-ball.offsetHeight || bally<=0)dy*=-1;
 
-    },1);
+    },15);
 
 
+    let paddley=250;
+    let py=5;
+    handle.style.top=`${paddley}px`;
 
-
-
+    document.addEventListener("keydown",(event)=>{
+        if(event.keyCode==40 && paddley<table.offsetHeight-handle.offsetHeight){
+            //DOWN
+            paddley+=py;
+        }
+        else if(event.keyCode==38 && paddley>0){
+            //UP
+            paddley+=(-1)*py;    
+        }
+        handle.style.top=`${paddley}px`;
+    });
 
 })
